3e34c1e903d2f15fa03e958007e8be91bc5ecf46
Daniel <kronkowski.daniel@gmail.com>
2023-01-09 21:43:14 +0100

    new functionality: use and move, add condition.txt and small improvements

--- escape_room.py
+++ escape_room.py
@@ -6,6 +6,7 @@ time_for_escape = timedelta(seconds= 600)
 path = Path(__file__).absolute().parent
 filepath = path / 'room.txt'
 
+
 def help():
     print('''
     Dostępne akcje:
@@ -24,7 +25,7 @@ def remaining_time():
     end_time = start_time + time_for_escape
     return str(end_time -datetime.now())
 
-
+#kod do poprawy - lista resetuje się po przejściu cyklu + gazetę i latarkę można zabrać tylko wtedy kiedy jest się przy stoliku
 class Inventory:
 
     def __init__(self, capacity=6):
@@ -57,42 +58,90 @@ class Room:
             for line in file.readlines():
                 print(line, end='')
 
-    def spot():
-        pass
+    def spot(self, place):
+        self.place = place
+        text = []
+        if place == "obraz" or 'stolik' or 'fotel':
+            try :
+                with open(path / (place+'.txt'), mode = 'r', encoding='utf-8') as file:
+                    for line in file.readlines():
+                        text.append(line)
+                    print(text[0])
+                with open(path / 'condition.txt', mode = 'w', encoding='utf-8') as file:
+                    file.write(place)
+            except:
+                print('''
+    Błędne miejsce!
+    Dostępne miejsca do których można podejść:
+    obraz
+    fotel
+    stolik
+    ''')
 
+def using(item):
+    text = []
+    if item == "gazeta":
+        with open(path / "gazeta.txt", mode = 'r', encoding='utf-8') as file:
+            for line in file.readlines():
+                print(line, end='')
+    elif item == "nóż":
+        with open(path / "condition.txt", mode = 'r', encoding='utf-8') as file:
+            if file.read() == "fotel":
+                with open(path / 'fotel.txt', mode = 'r', encoding='utf-8') as file:
+                    for line in file.readlines():
+                        text.append(line)
+                    print(text[3])
+            else:
+                print('Nie można tu użyć tego przedmiotu!')
+    elif item == "latarka UV":
+        with open(path / "condition.txt", mode = 'r', encoding='utf-8') as file:
+            if file.read() == "obraz":
+                with open(path / 'obraz.txt', mode = 'r', encoding='utf-8') as file:
+                    for line in file.readlines():
+                        text.append(line)
+                    print(text[3])
+            else:
+                print('Nie można tu użyć tego przedmiotu!')
 
 def action():
     choice = input('Wybierz akcje:')
+
     if choice == 'h':
-        return help()
+        return help(), action()
 
     elif choice == 'p':
-        return Inventory().__str__()
+        return Inventory().__str__(), action()
 
     elif choice == 'z':
         item = input("Wybierz przedmiot do zabrania: ")
-        return Inventory().get_item(item)
+        return Inventory().get_item(item), action()
 
     elif choice == 'w':
         item = input("Wybierz przedmiot do wyrzucenia: ")
-        return Inventory().throw_item(item)
+        return Inventory().throw_item(item), action()
 
     elif choice == "o":
-        return Room().__str__()
+        return Room().__str__(), action()
 
     elif choice == 't':
-        print(f"Tik, tak, tik, tak zostało Ci... {remaining_time()}")
+        print(f"Tik, tak, tik, tak zostało Ci... {remaining_time()}"), action()
 
     elif choice == 'm':
-        spot = input("Gdzie chcesz podejść: ")
-        return Room().spot(spot)
+        place = input("Gdzie chcesz podejść: ")
+        return Room().spot(place), action()
+
+    elif choice == "u":
+        item = input('Wybierz przedmiot do użycia: ')
+        return using(item), action()
 
     elif choice == 'e':
         print("przegrałeś")
+
     else:
-        print("Błędna akcja, spróbuj ponownie.")
-    
-    return action()
+        print("Błędna akcja, spróbuj ponownie."), help(), action()
     
     
+    
+Room().__str__()
+help()
 action()
